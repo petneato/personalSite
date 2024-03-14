15f6f5d832f911b3489f7f55fc0b2a596f0a3ed5
petneato <alecmontesano@gmail.com>
2024-03-14 17:26:46 -0400

    Sync for device switch

---
+++ personal-site/src/objects/project.js
@@ -0,0 +1,14 @@
+class project {
+    constructor(name, status, github, primaryLanguage, description, dates){
+       
+        this.name = name;
+        this.status = status;
+        this.github = github;
+        this.primaryLanguage = primaryLanguage;
+        this.description = description;
+        this.dates = dates;
+
+    }
+}
+
+var projectOne = new project()
---
+++ personal-site/src/objects/test.js
@@ -0,0 +1,21 @@
+const person1 = {
+    firstName: "Spongebob",
+    lastName: "Squarepants",
+    age: 30,
+    isEmployed: true,
+}
+
+const person2 = {
+    firstName: "Patrick",
+    lastName: "Star",
+    age: 42,
+    isEmployed: false,
+}
+
+console.log(person1.firstName);
+console.log(person1.lastName);
+console.log(person1.age);
+console.log(person1.firstName);
+console.log(person1.isEmployed)
+
+
